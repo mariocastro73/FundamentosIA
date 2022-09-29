6406df6b350768be3d5fc88b21aa09a697191147
Mario Castro <mariocastro73@users.noreply.github.com>
2022-09-29 16:47:36 +0200

    Template for linear regression (hand made)

---
+++ template_regression.py
@@ -0,0 +1,21 @@
+# Example of linear regression (manually)
+import numpy as np
+import matplotlib.pyplot as plt
+
+def estimate_coef(x, y):
+    """  """
+    return (b_0, b_1)
+
+def plot_regression_line(x, y, b):
+    """ """
+    # function to show plot
+    plt.show()
+
+def main():
+    """ """
+    # observations / data
+    x = np.array([0, 3, 2, 1, 4, 6, 7, 8, 9, 10])
+    y = np.array([2, 3, 2, 4, 5, 7, 9, 9, 10, 13])
+
+if __name__ == "__main__":
+    main()
